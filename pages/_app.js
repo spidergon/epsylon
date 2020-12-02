f2fa99ca42ef6bd4cfaee31b8e04a11096f879d5
spidergon <spidergon@gmail.com>
2020-12-02 13:32:17 -0300

    dev

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import 'modern-css-reset/dist/reset.css';
-import '../styles/globals.css';
+// import 'modern-css-reset/dist/reset.css';
+import '../styles/reset.css';
+import '../styles/main.css';
+// import '../styles/debug.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
